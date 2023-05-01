4f4ba8e98cc81009d18fa380683a0f400804e3f2
yurii-kovalchuk <sogdrums@gmail.com>
2023-05-01 14:52:49 +0300

    set up static

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
@@ -12,6 +13,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, "public", "avatars")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
